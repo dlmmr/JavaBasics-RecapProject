4ed7b11699cc3d1018fd26921cbfd065127dda92
Dennis Lemmer <dlemmer28@gmail.com>
2025-08-27 11:31:07 +0200

    added pwvalidation and some methods

---
+++ src/main/java/org/example/PasswordValidator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class PasswordValidator {
+    public static void main(String[] args) {}
+
+        public static boolean hasMinLength(String password, int min);
+
+        public static boolean containsDigit(String password);
+
+        public static boolean containsUpperAndLower(String password);
+
+        public static boolean isCommonPassword(String password); // kleine interne Liste
+
+}
